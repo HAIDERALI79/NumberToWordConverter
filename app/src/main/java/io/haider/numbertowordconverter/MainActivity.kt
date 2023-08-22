cd8729be996a012fef28f69b6547a437bf5c593a
haiderali <uchiha.usama@gmail.com>
2023-08-23 00:43:40 +0300

    adding copy icon in bottom left corner with card composable function

--- app/src/main/java/io/haider/numbertowordconverter/MainActivity.kt
+++ app/src/main/java/io/haider/numbertowordconverter/MainActivity.kt
@@ -1,4 +1,5 @@
 package io.haider.numbertowordconverter
+
 import android.annotation.SuppressLint
 import android.icu.util.Currency
 import android.os.Build
@@ -17,20 +18,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Paint
-import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
-import androidx.compose.ui.platform.ClipboardManager
-import androidx.compose.ui.platform.LocalClipboardManager
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
 import io.haider.numbertowordconverter.ui.theme.NumberToWordConverterTheme
 import java.util.Locale
 
@@ -52,6 +42,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 var number by mutableStateOf("")
 var arabicWord by mutableStateOf("")
 var englishWord by mutableStateOf("")
@@ -124,139 +115,33 @@ fun NumberToWord() {
             Spacer(modifier = Modifier.padding(16.dp))
             AppSwitches().Switches()
             Spacer(modifier = Modifier.padding(16.dp))
-            WordsCard()
+            WordsCard(wordString = "$arabicWord $dinarCurrencyState $dinarLastWord")
+            WordsCard(wordString = "$englishWord $dollarCurrencyState $dollarLastWord")
 
         }
     }
 }
 
-@Composable
-fun WordsCard() {
-    val clipboardManager: ClipboardManager = LocalClipboardManager.current
-    Column {
 
 
-        ConstraintLayout(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-                .shadows(
-                    borderRadius = 5.dp,
-                    blurRadius = 5.dp
 
-                )
-        ) {
-            val (content, fab) = createRefs()
-            Box(
-                modifier = Modifier.constrainAs(content) {
-                    top.linkTo(parent.top, margin = 16.dp)
+//fun Modifier.shadows(
+//    color: Color = Color.Black,
+//    borderRadius: Dp = 0.dp,
+//    blurRadius: Dp = 0.dp,
+//    modifier: Modifier = Modifier,
+//    spread: Dp = 0.dp
+//
+//) = this.then(
+//    drawBehind {
+//        this.drawIntoCanvas {
+//            val paint = Paint()
+//            val framePaint = paint.asFrameworkPaint()
+//            val spreadPixel = spread.toPx()
+//        }
+//    }
+//)
 
-                }
-            ) {
-                Text(
-                    text = "$arabicWord $dinarCurrencyState $dinarLastWord",
-                    style = MaterialTheme.typography.bodyMedium
-                )
-            }
-            FloatingActionButton(
-                modifier = Modifier
-                    .defaultMinSize(
-                        minWidth = 40.dp,
-                        minHeight = 40.dp,
-                    )
-                    .constrainAs(fab) {
-                        end.linkTo(parent.end, margin = 8.dp)
-                        bottom.linkTo(parent.bottom, margin = 8.dp)
-                        top.linkTo(content.bottom)
-                    },
-                onClick = {
-                    clipboardManager.setText(
-                        annotatedString = AnnotatedString(
-                            "$arabicWord $dinarCurrencyState $dinarLastWord"
-                        )
-                    )
-                },
-                shape = ShapeDefaults.ExtraLarge,
-                elevation = FloatingActionButtonDefaults.elevation()
-
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.content_copy),
-                    contentDescription = "copy"
-                )
-
-
-            }
-        }
-        ConstraintLayout(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-                .shadow(
-                    elevation = 2.dp
-                )
-        ) {
-            val (text, button) = createRefs()
-            Box(modifier = Modifier
-                .padding(8.dp)
-                .constrainAs(text) {
-                    top.linkTo(parent.top)
-                }) {
-                Text(
-                    text = "$englishWord $dollarCurrencyState $dollarLastWord",
-                    style = MaterialTheme.typography.bodyMedium
-                )
-            }
-            FloatingActionButton(
-                modifier = Modifier
-                    .defaultMinSize(
-                        minWidth = 40.dp,
-                        minHeight = 40.dp,
-                    )
-                    .constrainAs(button) {
-                        end.linkTo(parent.end, margin = 8.dp)
-                        bottom.linkTo(parent.bottom, margin = 8.dp)
-                        top.linkTo(text.bottom)
-                    },
-                onClick = {
-                    clipboardManager.setText(
-                        annotatedString = AnnotatedString(
-                            "$englishWord $dollarCurrencyState $dollarLastWord"
-                        )
-                    )
-                },
-                shape = ShapeDefaults.ExtraLarge,
-                elevation = FloatingActionButtonDefaults.elevation()
-
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.content_copy),
-                    contentDescription = "copy"
-                )
-
-
-            }
-        }
-    }
-}
-
-
-fun Modifier.shadows(
-    color: Color = Color.Black,
-    borderRadius: Dp =0.dp,
-    blurRadius:Dp=0.dp,
-    modifier: Modifier=Modifier,
-    spread:Dp=0.dp
-
-)=this.then(
-    drawBehind {
-this.drawIntoCanvas {
-    val paint= Paint()
-    val framePaint=paint.asFrameworkPaint()
-    val spreadPixel=spread.toPx()
-}
-    }
-)
 @RequiresApi(Build.VERSION_CODES.TIRAMISU)
 @Preview(showBackground = true)
 @Composable
@@ -264,6 +149,5 @@ fun DefaultPreview() {
     NumberToWordConverterTheme {
         NumberToWord()
         // AppSwitches()
-        //   WordsCard()
     }
 }
